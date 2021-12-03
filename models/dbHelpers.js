7e1ca1a151d2b694ead7be6c76acc60ec5c95e26
patrick_hewes <patrick.hewes@centeva.com>
2021-12-03 15:34:14 -0600

    Add update route from Lesson 11

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -7,7 +7,8 @@ module.exports = {
     add,
     find,
     findById,
-    remove
+    remove,
+    update
 };
 
 async function add(lesson) {
@@ -30,4 +31,13 @@ function remove(id) {
     return db("lessons")
         .where({ id })
         .del();
+}
+
+function update(id, changes) {
+    return db("lessons")
+        .where({ id })
+        .update(changes, [id])
+        .then(() => {
+            return findById(id);
+        });
 }
